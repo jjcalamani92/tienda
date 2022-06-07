be554156fdd83d2e724c90ce6d9775479411fdab
Jesus Calamani <jesuscalamani92@gmail.com>
2022-06-06 22:15:16 -0400

    yyyyyyy

---
+++ pages/contacto.tsx
@@ -0,0 +1,16 @@
+import { Layout } from "../components";
+
+
+const ContactoPage = () => {
+	return (
+		<Layout
+			title={"Piccoletti - Categorias"}
+			pageDescription={"Encuentra tu ropa favorita"}
+		>
+			<h1>pagina de contácto</h1>
+		</Layout>
+	);
+};
+
+
+export default ContactoPage;
---
+++ pages/servicios.tsx
@@ -0,0 +1,16 @@
+import { Layout } from "../components";
+
+
+const ServicioPage = () => {
+	return (
+		<Layout
+			title={"Piccoletti - Categorias"}
+			pageDescription={"Encuentra tu ropa favorita"}
+		>
+			<h1>pagina de Servicios</h1>
+		</Layout>
+	);
+};
+
+
+export default ServicioPage;
