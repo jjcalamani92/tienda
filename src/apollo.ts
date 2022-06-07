c7d490cee015a4c65ed018f7cbc5ff60aac39edd
Jesus Calamani <jesuscalamani92@gmail.com>
2022-06-06 21:54:13 -0400

    yyyyyy

--- src/apollo.ts
+++ src/apollo.ts
@@ -7,8 +7,8 @@ import {
 export const client = new ApolloClient({
 	cache: new InMemoryCache(),
   link: new HttpLink({
-    // uri: 'https://cristinadevelopmentp.herokuapp.com/graphql',
-    uri: `${process.env.APIP_URL}/graphql`,
+    uri: 'https://cristinadevelopmentp.herokuapp.com/graphql',
+    // uri: `${process.env.APIP_URL}/graphql`,
     fetch
   }),
   connectToDevTools: true,
@@ -16,8 +16,8 @@ export const client = new ApolloClient({
 export const clientSite = new ApolloClient({
 	cache: new InMemoryCache(),
   link: new HttpLink({
-    // uri: 'https://cristinadevelopments.herokuapp.com/graphql',
-    uri: `${process.env.APIS_URL}/graphql`,
+    uri: 'https://cristinadevelopments.herokuapp.com/graphql',
+    // uri: `${process.env.APIS_URL}/graphql`,
     // uri: 'http://localhost:8002/graphql',
     fetch
   }),
