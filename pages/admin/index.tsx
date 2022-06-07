a6c6bb430391aa817fd7d879204cde9c352bd306
Jesus Calamani <jesuscalamani92@gmail.com>
2022-06-06 20:48:41 -0400

    yyyyy

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,13 +2,13 @@ import { NextPage } from "next";
 import { Layout } from "../../components";
 import { Table02 } from "../../components/Table";
 import { client } from "../../src/apollo";
-import { PRODUCT_UPDATE, PRODUCTS } from '../../src/gql/query';
+import { PRODUCT_UPDATE, PRODUCTS } from "../../src/gql/query";
 import { IProduct, Product } from "../../src/interfaces";
 interface Props {
-	wears: IProduct[];
+	paints: IProduct[];
 }
 
-const AdminPage: NextPage<Props> = ({ wears }) => {
+const AdminPage: NextPage<Props> = ({ paints }) => {
 	// console.log('wears')
 	// console.log(wears)
 	return (
@@ -16,9 +16,8 @@ const AdminPage: NextPage<Props> = ({ wears }) => {
 			title={"Choco - Stores"}
 			pageDescription={"Encuentra tu ropa favorita"}
 		>
-      {/* <Table01 products={wears} /> */}
-      <Table02 products={wears} />
-
+			{/* <Table01 products={wears} /> */}
+			<Table02 products={paints} />
 		</Layout>
 	);
 };
@@ -29,7 +28,7 @@ export async function getStaticProps() {
 	});
 	return {
 		props: {
-			wears: data.wears
+			paints: data.paints
 		}
 	};
 }
