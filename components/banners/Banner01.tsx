5e33f012a9b13e7aacf0591fd80b3a2d4c7c2f2c
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-06-12 02:34:22 +0000

    v

--- components/banners/Banner01.tsx
+++ components/banners/Banner01.tsx
@@ -12,7 +12,7 @@ export const Banner01 = () => {
             </span>
             <p className="ml-3 font-medium text-white truncate">
               <span className="md:hidden">We announced a new product!</span>
-              <span className="hidden md:inline">Big news! We're excited to announce a brand new product.</span>
+              <span className="hidden md:inline">Big news! Were excited to announce a brand new product.</span>
             </p>
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
